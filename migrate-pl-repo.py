1d94afa541f4882ebf2079e29e65d881db65e33c
Andrew Stec <andrewstec@gmail.com>
2020-10-26 14:47:14 -0700

    Introducing repo creation if it does not exist on migration, and fixing api token dest bug

--- migrate-pl-repo.py
+++ migrate-pl-repo.py
@@ -12,6 +12,7 @@
 
 # Imports
 import os
+from re import fullmatch
 import sys
 import re
 import time
@@ -28,16 +29,34 @@ token_dest = ''
 headers_source = {}
 headers_dest = {}
 
+print('Github migrations require a source and destination repository to work with. Please enter the source and destination repositories in Https format (ie. https://api_token@github.enterprise.instance/org_or_owner/repo_name')
+full_source_url = input('Source repository: ').strip()
+full_dest_url = input('Destination repository: ').strip()
+params_source = full_source_url.split('/')
+params_dest = full_dest_url.split('/')
+
+github_source = params_source[0] + '//' + params_source[2] + '/api/v3/'
+github_dest = params_dest[0] + '//' + params_dest[2] + '/api/v3/'
+repo_source = params_source[4]
+repo_dest = params_dest[4]
+org_name_source = params_source[3]
+org_name_dest = params_dest[3]
+api_token_source = params_source[2].split('@')[0]
+api_token_dest = params_dest[2].split('@')[0]
+
+if len(api_token_source) == 0 or len(api_token_dest) == 0:
+    raise Exception('API token must be defined for source and destination repositories.') 
+
 def main():
 
     print('This tool assists with common UBC migration tasks. Each operation is disjointed and optional, but all of these operations may be combined if desired. Option selections can be chosen based on a particular use-case in mind.\n\n')
 
     print('User migration requirements: ')
-    print(' - Admin priviledges in source and destination repositories')
-
+    print(' - Admin priviledges in source and destination repositories\n')
+    print(' - Must have SSH Keys installed to SSH cloning\n')
     print('Repository creation/data transfer requirements: ')
     print(' - Owner priviliges in Organization storing repositories\n\n')
-    
+
     print('1. Migrate Users')
     print('2. Migrate Repositories')
     print('3. Exit')
@@ -54,14 +73,26 @@ def main():
         main()
 
 def clone_source_repo():
-    repo_address = input('Enter the source repository address to copy: ').strip()
+    # repo_address = input('Enter the source repository address to copy: ').strip()
     os.system('echo Removing any pre-existing migration data')
     Popen(['rm', '-rf', SOURCE_FS_PATH])
-    Popen(['git', 'clone', '--mirror', repo_address, SOURCE_FS_PATH]).wait()
+    Popen(['git', 'clone', '--mirror', full_source_url, SOURCE_FS_PATH]).wait()
 
 def push_dest_repo():
-    dest_repo_address = input('Enter the destination repository address: ').strip()
-    Popen(['git', 'push', '--mirror', dest_repo_address], cwd=SOURCE_FS_PATH).wait()
+    headers_source = set_header(api_token_source)
+    headers_dest = set_header(api_token_dest)
+
+    source_repo = get_repo(github_source, org_name_source, repo_source, headers_source)
+
+    ## Can customize options to migrate: https://developer.github.com/v3/repos/#create-an-organization-repository
+    options = {'private': source_repo['private'], 'description': source_repo['description']}
+    dest_repo = get_repo(github_dest, org_name_dest, repo_dest, headers_dest)
+    if dest_repo is not None: 
+        Popen(['git', 'push', '--mirror', full_dest_url], cwd=SOURCE_FS_PATH).wait()
+    else:
+        create_repo(github_dest, org_name_dest, repo_dest, headers_dest, options)
+        time.sleep(2)
+        Popen(['git', 'push', '--mirror', full_dest_url], cwd=SOURCE_FS_PATH).wait()
 
 def get_org(address):
     return repo_address_dest
@@ -69,20 +100,27 @@ def get_org(address):
 def migrate_users():
     ## This can all be greatly simplified by parsing a URL link with a token included
     print('This operation copies the collaborators and their privileges on a repository to a new repository.')
-    params_source = input('Enter the repo to copy users from (https://api_token@github.enterprise.instance/org_or_owner/repo_name): ').strip().split('/')
-    params_dest = input('Enter the repo to copy users from (https://api_token@github.enterprise.instance/org_or_owner/repo_name): ').strip().split('/')
+    # params_source = input('Enter the repo to copy users from (https://api_token@github.enterprise.instance/org_or_owner/repo_name): ').strip().split('/')
+    # params_dest = input('Enter the repo to copy users from (https://api_token@github.enterprise.instance/org_or_owner/repo_name): ').strip().split('/')
 
-    github_source = params_source[0] + '//' + params_source[2] + '/api/v3/'
-    github_dest = params_dest[0] + '//' + params_dest[2] + '/api/v3/'
-    repo_source = params_source[4]
-    repo_dest = params_dest[4]
-    org_name_source = params_source[3]
-    org_name_dest = params_dest[3]
-    api_token_source = params_source[2].split('@')[0]
-    api_token_dest = params_source[2].split('@')[0]
+    # github_source = params_source[0] + '//' + params_source[2] + '/api/v3/'
+    # github_dest = params_dest[0] + '//' + params_dest[2] + '/api/v3/'
+    # repo_source = params_source[4]
+    # repo_dest = params_dest[4]
+    # org_name_source = params_source[3]
+    # org_name_dest = params_dest[3]
+    # api_token_source = params_source[2].split('@')[0]
+    # api_token_dest = params_source[2].split('@')[0]
 
     headers_source = set_header(api_token_source)
     headers_dest = set_header(api_token_dest)
+
+    # If repo does not already exist, it should be created with same permissions
+    source_repo = get_repo(github_source, org_name_source, repo_source, headers_source)
+    dest_repo = get_repo(github_dest, org_name_source, repo_source, headers_dest)
+    options = {}
+    if dest_repo is not None:
+        create_repo(github_source, org_name_dest, repo_dest, headers_dest)
 
     users_source_list = get_users(github_source, org_name_source, repo_source, headers_source)
     users_dest_list = get_users(github_dest, org_name_dest, repo_dest, headers_dest)
@@ -114,6 +152,10 @@ def migrate_users():
     print('Complete')
     sys.exit(0)
 
+def copy_repo(copy_settings = True):
+    source_repo_data = get_repo(github_source, org_name_source, repo_source, headers_source)
+    res = create_repo(github_dest, org_name_dest, repo_dest, headers_dest, source_repo_data)
+
 def get_users(github_domain, org_name, repo, headers):
     endpoint_url = urljoin(github_domain, 'repos/{0}/{1}/contributors'.format(org_name, repo))
     json = request(endpoint_url, headers).json()
@@ -131,6 +173,21 @@ def add_user_to_repo(github_domain, org_name, repo_name, user_login, user_role, 
     endpoint_url = urljoin(github_domain, 'repos/{0}/{1}/collaborators/{2}'.format(org_name, repo_name, user_login))
     res = request(endpoint_url, headers, 'PUT', data={'permission': user_role})
     return res
+
+
+def create_repo(github_domain, org_name, repo_name, headers, options = {}):
+    options['name'] = repo_name
+    endpoint_url = urljoin(github_domain, 'orgs/{0}/repos').format(org_name)
+    res = request(endpoint_url, headers, 'POST', options).json()
+    return res
+
+def get_repo(github_domain, org_name, repo_name, headers):
+    endpoint_url = urljoin(github_domain, 'repos/{0}/{1}').format(org_name, repo_name)
+    res = request(endpoint_url, headers)
+    if res.status_code == 200:
+        return res.json()
+    else:
+        return None
 
 def set_header(api_token):
 	return {'Content-Type': 'application/json',
